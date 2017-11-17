a80e80e753fd4378685a5035fee459a257bdc4e6
maoow <corentin34530@hotmail.fr>
2017-11-17 21:30:53 +0100

    structs

--- includes/fdf.h
+++ includes/fdf.h
@@ -22,8 +22,38 @@ typedef struct	s_fdfenv
 	size_t	height;
 	size_t	x;
 	size_t	y;
+	void	*img;
+	unsigned int	*imgstr;
 }	t_fdfenv;
+
+int			keypressed(int key, t_fdfenv *env);
+int			buttonpressed(int key,int x, int y, t_fdfenv *env);
 void			right(t_fdfenv *env);
 void			down(t_fdfenv *env);
 void			up(t_fdfenv *env);
 void			left(t_fdfenv *env);
+
+typedef struct	s_point
+{
+	size_t	x;
+	size_t	y;
+	size_t	z;
+}		t_point;
+
+typedef struct	s_vertex
+{
+	t_point	a;
+	t_point	b;
+}		t_vertex;
+
+typedef struct	s_pixel
+{
+	size_t	x;
+	size_t	y;
+}		t_pixel;
+
+typedef struct	s_line
+{
+	t_pixel	a;
+	t_pixel	b;
+}		t_line;
