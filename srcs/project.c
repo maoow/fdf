780dc60865273d6e6531239781a194d87b446076
maoow <corentin34530@hotmail.fr>
2017-11-18 02:30:58 +0100

    correction + zoom

--- srcs/project.c
+++ srcs/project.c
@@ -3,16 +3,18 @@
 
 void		project(t_fdfenv *env, t_point point, t_pixel *pixel)
 {
-	pixel->x = -env->camera.a.x * (point.x - (env->camera.a.x));
-	pixel->y = -env->camera.a.y * (point.y - (env->camera.a.y));
-	pixel->x *= 1;
-	pixel->y *= 1;
+	pixel->x = env->camera.a.z * (point.x + (env->camera.a.x));
+	pixel->y = env->camera.a.z * (point.y + (env->camera.a.y));
 ft_printf("%d %d\n",pixel->x,pixel->y);
 	if (env->camera.a.z + point.z)
 	{
 		pixel->x /= env->camera.a.z + point.z;
 		pixel->y /= env->camera.a.z + point.z;
 	}
+	pixel->x *=  20;
+	pixel->y *=  20;
+	pixel->x += env->width / 2;
+	pixel->y += env->height / 2;
 }
 
 void		drawpoint(t_fdfenv *env)
