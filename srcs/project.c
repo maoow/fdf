4f2b92c9e66f4fb438667aa942b8f1427647417a
maoow <corentin34530@hotmail.fr>
2017-11-18 15:08:08 +0100

    corrections

--- srcs/project.c
+++ srcs/project.c
@@ -3,16 +3,16 @@
 
 void		project(t_fdfenv *env, t_point point, t_pixel *pixel)
 {
-	pixel->x = env->camera.a.z * (point.x + (env->camera.a.x));
-	pixel->y = env->camera.a.z * (point.y + (env->camera.a.y));
+	pixel->x = env->camera.a.z * (point.x - (env->camera.a.x));
+	pixel->y = env->camera.a.z * (point.y - (env->camera.a.y));
 //ft_printf("%d %d\n",pixel->x,pixel->y);
 	if (env->camera.a.z + point.z)
 	{
 		pixel->x /= env->camera.a.z + point.z;
 		pixel->y /= env->camera.a.z + point.z;
 	}
-	pixel->x *=  20;
-	pixel->y *=  20;
+	pixel->x *=  env->zoom;
+	pixel->y *=  env->zoom;
 	pixel->x += env->width / 2;
 	pixel->y += env->height / 2;
 }
