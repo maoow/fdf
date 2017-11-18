8d31c7893bbaf29db1696e9204dfb07790c41cd5
maoow <corentin34530@hotmail.fr>
2017-11-18 14:44:32 +0100

    drawpoint function move

--- srcs/project.c
+++ srcs/project.c
@@ -5,7 +5,7 @@ void		project(t_fdfenv *env, t_point point, t_pixel *pixel)
 {
 	pixel->x = env->camera.a.z * (point.x + (env->camera.a.x));
 	pixel->y = env->camera.a.z * (point.y + (env->camera.a.y));
-ft_printf("%d %d\n",pixel->x,pixel->y);
+//ft_printf("%d %d\n",pixel->x,pixel->y);
 	if (env->camera.a.z + point.z)
 	{
 		pixel->x /= env->camera.a.z + point.z;
@@ -17,31 +17,3 @@ ft_printf("%d %d\n",pixel->x,pixel->y);
 	pixel->y += env->height / 2;
 }
 
-void		drawpoint(t_fdfenv *env)
-{
-	int	i;
-	int	j;
-	t_pixel	tmp;
-	t_pixel	tmp2;
-	i = 0;
-	while (i < env->mapsize.y)
-	{
-		j = 0;
-		while (j < env->mapsize.x)
-		{
-			project(env, env->map[i][j], &tmp);
-			if (i < env->mapsize.y - 1)
-			{
-				project(env, env->map[i + 1][j], &tmp2);
-drawline(env, tmp,tmp2);
-			}
-			j++;
-			if (j < env->mapsize.x)
-			{
-				project(env, env->map[i][j], &tmp2);
-drawline(env, tmp,tmp2);
-			}
-		}
-		i++;
-	}
-}
