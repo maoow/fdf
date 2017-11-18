248e39c0a33a0a484550181a8d6ff089abf4ff18
maoow <corentin34530@hotmail.fr>
2017-11-18 01:12:02 +0100

    multiply height

--- srcs/move.c
+++ srcs/move.c
@@ -6,11 +6,37 @@ void			left(t_fdfenv *env)
 }
 void			up(t_fdfenv *env)
 {
-	env->y--;
+	int	i;
+	int	j;
+	i = 0;
+	while (i < env->mapsize.y)
+	{
+		j = 0;
+		while (j < env->mapsize.x)
+		{
+			env->map[i][j].z *= 2;
+			j++;
+		}
+		i++;
+	}
+drawpoint(env);
 }
 void			down(t_fdfenv *env)
 {
-	env->y++;
+	int	i;
+	int	j;
+	i = 0;
+	while (i < env->mapsize.y)
+	{
+		j = 0;
+		while (j < env->mapsize.x)
+		{
+			env->map[i][j].z /= 2;
+			j++;
+		}
+		i++;
+	}
+drawpoint(env);
 }
 void			right(t_fdfenv *env)
 {
