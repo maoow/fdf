5c9f42ac4fd01ff50f468a055e9a479f10717a19
maoow <corentin34530@hotmail.fr>
2017-11-18 18:57:23 +0100

    - height

--- srcs/project.c
+++ srcs/project.c
@@ -1,18 +1,19 @@
 #include "fdf.h"
 
-
 void		project(t_fdfenv *env, t_point point, t_pixel *pixel)
 {
 	pixel->x = env->camera.a.z * (point.x - (env->camera.a.x));
 	pixel->y = env->camera.a.z * (point.y - (env->camera.a.y));
 //ft_printf("%d %d\n",pixel->x,pixel->y);
-	if (env->camera.a.z + point.z)
+	if (env->camera.a.z - point.z)
 	{
-		pixel->x /= env->camera.a.z + point.z;
-		pixel->y /= env->camera.a.z + point.z;
+		pixel->x /= env->camera.a.z - point.z;
+		pixel->y /= env->camera.a.z - point.z;
 	}
 	pixel->x *=  env->zoom;
 	pixel->y *=  env->zoom;
+	pixel->x /=  25;
+	pixel->y /=  25;
 	pixel->x += env->width / 2;
 	pixel->y += env->height / 2;
 }
