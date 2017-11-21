826805effee633176a3b9ad05fdb13446662e15d
root <root@maoow.localdomain>
2017-11-21 18:15:17 +0100

    point selection with mouse

--- srcs/move.c
+++ srcs/move.c
@@ -6,64 +6,86 @@ void			mouserotate(t_fdfenv *env)
 	{
 		env->rotate.z -= ((float)env->mouse.x - (float)env->width / 2) / env->width;
 		env->rotate.x += ((float)env->mouse.y - (float)env->height / 2) / env->height;
-		drawpoint(env);
 	}
 }
 void			center(t_fdfenv *env)
 {
 	env->camera.a.x += env->mouse.x - env->width / 2;
 	env->camera.a.y += env->mouse.y - env->height / 2;
-	drawpoint(env);
+}
+void			selectpoint(t_fdfenv *env)
+{
+	t_pixel tmp;
+	t_pixel diff;
+	int	i;
+	int	j;
+
+	i =0 ;
+	while (i < env->mapsize.y)
+	{
+		j = 0;
+		while (j < env->mapsize.x)
+		{
+			project(env, env->map[i][j], &tmp);
+			diff.x = env->mouse.x - tmp.x;
+			diff.y = env->mouse.y - tmp.y;
+			if (diff.x <  5 && diff.x > -5 && diff.y < 5 && diff.y > -5)
+			{
+				env->x = i;
+				env->y = j;
+			}
+			j++;
+		}
+		i++;
+	}
 }
 void			left(t_fdfenv *env)
 {
 	env->camera.a.x+= 10;
-	drawpoint(env);
 }
 void			up(t_fdfenv *env)
 {
 	env->camera.a.y+= 10;
-	drawpoint(env);
 }
 void			down(t_fdfenv *env)
 {
 	env->camera.a.y-= 10;
-	drawpoint(env);
 }
 void			right(t_fdfenv *env)
 {
 	env->camera.a.x-= 10;
-	drawpoint(env);
+}
+void			higherpoint(t_fdfenv *env)
+{
+	env->map[env->x][env->y].z++;
 }
 void			higher(t_fdfenv *env)
 {
 	env->top++;
-	drawpoint(env);
+}
+void			lowerpoint(t_fdfenv *env)
+{
+	env->map[env->x][env->y].z--;
 }
 void			lower(t_fdfenv *env)
 {
 	env->top--;
-	drawpoint(env);
 }
 void			zoom(t_fdfenv *env)
 {
 	env->zoom++;
-	drawpoint(env);
 }
 void			dezoom(t_fdfenv *env)
 {
 	if (env->zoom > 1)
 		env->zoom--;
-	drawpoint(env);
 }
 void			harder(t_fdfenv *env)
 {
 	if (env->smooth > 1)
 		env->smooth--;
-	drawpoint(env);
 }
 void			smoother(t_fdfenv *env)
 {
 	env->smooth++;
-	drawpoint(env);
 }
